34fe3790200f577e84ed4f52e3f63e62fd52e1a7
arumandesu <zh.arumandes@gmail.com>
2023-09-21 22:02:25 +0600

    Animation

--- app/src/main/java/com/example/animejourney/AnimeTipScreen.kt
+++ app/src/main/java/com/example/animejourney/AnimeTipScreen.kt
@@ -1,7 +1,12 @@
 package com.example.animejourney
 
 import android.content.res.Configuration
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,12 +17,17 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.animejourney.model.AnimeTip
 import com.example.animejourney.model.AnimeTipsRepository
 import com.example.animejourney.ui.theme.AnimeJourneyTheme
@@ -33,15 +43,24 @@ fun AnimeTipItem(
     tip:AnimeTip,
     modifier: Modifier = Modifier,
 ){
+    var extended by remember { mutableStateOf(false) }
     Card(
-        modifier = modifier,
+        modifier = modifier.clickable { extended = !extended },
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.tertiaryContainer,
             contentColor = MaterialTheme.colorScheme.onTertiaryContainer
-        )
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
         Column(
-            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
         ) {
             Text(
                 text = stringResource(tip.monthRes),
@@ -67,11 +86,14 @@ fun AnimeTipItem(
                     modifier = Modifier.fillMaxWidth(),
                 )
             }
+            if(extended){
+                Text(
+                    text = stringResource(tip.descriptionRes),
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
+                )
+            }
 
-            Text(
-                text = stringResource(tip.descriptionRes),
-                style = MaterialTheme.typography.bodySmall
-            )
         }
     }
 }
